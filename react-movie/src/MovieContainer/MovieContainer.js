e3fe5202c26aaa52122b34fd424068beb5917707
Jack Bogart <bogart.jack98@gmail.com>
2018-11-01 11:16:32 -0600

    working

---
+++ react-movie/src/MovieContainer/MovieContainer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class MovieContainer extends Component{
+	constructor(){
+		super();
+		this.state = {
+			movies: []
+		}
+	}
+	render() {
+		return(
+			<div> this is working </div>
+		)
+	}
+}
+
+export default MovieContainer
